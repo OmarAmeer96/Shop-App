6085eea489e82be8bbb110cbf8cc057c6b34bb6b
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-18 22:06:06 +0300

    Added the splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/views/home_view.dart';
+import 'package:shop_app/views/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Shop App',
       theme: ThemeData(
         brightness: Brightness.dark,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
-      home: const HomeView(),
+      routes: {
+        HomeView.id: (context) => const HomeView(),
+      },
+      initialRoute: SplashView.id,
     );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  static String id = "HomeView";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
