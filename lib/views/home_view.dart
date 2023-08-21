2731ec1930ca67d29a8f899103a6e50755c4a1cb
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-21 17:16:19 +0300

    Change to Android Studio :(

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/consts.dart';
+
 import '../widgets/build_app_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -13,13 +14,36 @@ class HomeView extends StatelessWidget {
       backgroundColor: backColor,
       appBar: buildAppBar(),
       body: Center(
-        child: Container(
-          height: screenHeight * 0.13,
-          width: screenWidth * 0.45,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: primaryColor,
-          ),
+        child: Column(
+          children: [
+            Center(
+              child: Card(
+                color: primaryColor,
+                child: const Column(
+                  children: [
+                    Text("HandBag LV"),
+                    Row(
+                      children: [
+                        Text("\$225"),
+                        Spacer(),
+                        Icon(Icons.favorite),
+                        Icon(Icons.abc),
+                        Icon(Icons.abc_sharp),
+                        Icon(Icons.access_alarm_outlined),
+                        Icon(Icons.abc),
+                        Icon(Icons.create_new_folder_sharp),
+                      ],
+                    )
+                  ],
+                ),
+                //       height: screenHeight * 0.13,
+                // width: screenWidth * 0.45,
+                // decoration: BoxDecoration(
+                //   borderRadius: BorderRadius.circular(16),
+                //   color: primaryColor,
+              ),
+            )
+          ],
         ),
       ),
     );
