96fa6abf6ea9cfa14ce050bf1fea037949d5b1ad
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-23 13:51:01 +0300

    Product detail view done

--- lib/views/product_detail_view.dart
+++ lib/views/product_detail_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/widgets/custom_main_button.dart';
 
 import '../models/product_model.dart';
 
@@ -10,24 +11,81 @@ class ProductDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back_ios),
-          color: Colors.black,
-        ),
-        backgroundColor: Colors.white,
-        title: const Text(
-          "Product Details",
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            color: Colors.black,
+      appBar: buildProductDetailAppBar(context),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: screenWidth * 0.6,
+                  height: screenHeight * 0.6,
+                  child: Image.network(
+                    product.image,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.0,
+                ),
+                Text(
+                  product.title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: "Gilroy-Bold",
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.015,
+                ),
+                Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 15,
+                    fontFamily: "Gilroy-Medium",
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.025,
+                ),
+                CustomMainButton(text: "Buy Now", onPressed: () {}),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                )
+              ],
+            ),
           ),
+        ),
+      ),
+    );
+  }
+
+  AppBar buildProductDetailAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back_ios),
+        color: Colors.black,
+      ),
+      backgroundColor: Colors.white,
+      title: const Text(
+        "Product Details",
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          color: Colors.black,
         ),
       ),
     );
